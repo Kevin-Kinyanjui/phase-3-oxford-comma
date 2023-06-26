4dee73225641d2207d8938bbff610b33d7da49e3
Kevin-kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-06-26 12:26:18 +0300

    assignment complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 0
+    ""
+  elsif array.size == 1
+    array[0]
+  elsif array.size == 2
+    array.join(" and ")
+  else
+    last_element = array.pop
+    array_str = array.join(", ")
+    array_str + ", and " + last_element
+  end
 end
+
